9766683c16107670243d9fcea2e6ee502e1059c7
Sai Kaung Khant <kaungkaung.m@gmail.com>
2019-06-07 15:02:51 +0630

    [NPM INSTALL] added material-ui/icon, added router and dashboard file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Dashboard from "pages/dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Here
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route render={props => <Dashboard {...props} />} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
